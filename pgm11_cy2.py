3b50d32c65e679b43fbf382b3604edfb16892696
niftamoljose <74651803+niftamoljose@users.noreply.github.com>
2021-12-06 12:26:49 +0530

    added

---
+++ pgm11_cy2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  6 12:21:28 2021
+
+@author: sjcet
+"""
+
+import numpy as np
+
+array_1d=np.array([1,2,3,4,5])
+x=np.sum(array_1d)
+print(array_1d)
+print("the use of sum() function in 1D array",x)
+
+array_2d=np.array([[[1,2,3],[4,5,6],[7,8,9]]])
+y=np.sum(array_2d)
+print("\n",array_2d)
+print("the use of sum() function in 1D array",y)
