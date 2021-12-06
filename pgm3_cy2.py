585b35dace70cb7cbe0aaae72530fe675d812393
niftamoljose <74651803+niftamoljose@users.noreply.github.com>
2021-12-06 11:04:32 +0530

    Add files via upload

---
+++ pgm3_cy2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  6 10:54:10 2021
+
+@author: sjcet
+"""
+
+import numpy as np
+
+a1=np.empty(3,dtype=int,order='c')
+print(a1)
---
+++ prgm4_cy2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 24 12:08:39 2021
+
+@author: sjcet
+"""
+
+import numpy as np
+
+array_1d=np.array([1,2,3,4,5,6,7,8,9,10])
+print("First 4 elements ",array_1d[:4])
+print("Last 6 elements ",array_1d[4:])
+print("Elements from index 2 to 7 ",array_1d[2:7])
